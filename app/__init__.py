9192eea43286f8c53daae55b19f998c768bb005a
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-15 14:04:08 +0300

    creat the application script and pass app and database instance

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,10 @@ import sqlalchemy
 from config import config_options
 
 
-db = sqlalchemy()
+db = SQLAlchemy()
 
 def create_app(config_name):
-  
+
     app = Flask(__name__)
 
     app.config.from_object(config_options[config_name])
