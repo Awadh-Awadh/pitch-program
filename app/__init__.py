40794a9574d03a609a4e2fe1bb0c69e2cdd19974
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-15 15:20:02 +0300

    create the index route and initialize bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy
 from config import config_options
+from flask_bootstrap import Bootstrap
 
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
 
 def create_app(config_name):
 
@@ -19,7 +21,8 @@ def create_app(config_name):
     #Register authentication Blueprint
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
-
+    #initializing bootstrap
+    bootstrap.init_app(app)
     # initializing SQLAlchemy
     db.init_app(app)
 
