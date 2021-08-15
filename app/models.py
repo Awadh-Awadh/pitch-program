354f54ce30df13e5299970a595695d4ef8f1567f
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-15 19:44:19 +0300

    create a table for pitches

--- app/models.py
+++ app/models.py
@@ -1,13 +1,19 @@
 from app import db
 
-
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    pitch = db.column(db.String(255))
+    upvote = db.Column(db.Integer)
+    downvote = db.Column(db.Integer)
+    def __repr__(self):
+        return '<User %r>' % self.pitch
 
 
 
 class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(50), unique = True, nullable = False)
-    email = db.Column(db.String(), unique = True, nullable = False)
+    email = db.Column(db.String(255), unique = True, nullable = False)
     
     
     def __repr__(self):
