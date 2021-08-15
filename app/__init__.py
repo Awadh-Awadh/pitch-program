40e5ca7ecc5edba77b68046bb173e1a56cea313a
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-15 13:43:40 +0300

    add authentication blueprint and registered it in the app

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,12 @@ app = Flask(__name__)
 def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
-    #registering Blueprint
+    #registering the main Blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    #Register authentication Blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+
     return app
