5e18bec486a53be2ebfc9657edcbbba2047b1b49
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-19 23:56:37 +0300

    add flashed messages

--- config.py
+++ config.py
@@ -3,8 +3,9 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard stuff to to crack'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
-    MAIL_SERVER = 'smtp.google.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
