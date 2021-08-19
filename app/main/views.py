f0c5bbebb3970fecd558294f08d02e5031542cf4
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-08-19 20:29:13 +0300

    add styling to the login and logout forms

--- app/main/views.py
+++ app/main/views.py
@@ -11,8 +11,8 @@ def index():
 
 @main.route('/pitches',methods=['GET', 'POST'])
 def pitches():
-  upvote = 2
-  downvote = 1
+  upvote = 0
+  downvote = 0
   pitches = [
     {
       "pitch": "I recently launched a website for people who like to work on classic cars.      The website has articles and videos with tutorials on how to work on cars",
@@ -33,16 +33,15 @@ def pitches():
       db.session.commit()
       return redirect(url_for('.pitches'))
   posts = Pitch.query.all()
-  return render_template('pitches.html', posts =posts , pitches = pitches, pitch_form = pitch_form)
+  return render_template('pitches.html', posts =posts , pitches = pitches, pitch_form = pitch_form, upvote = upvote, downvote = downvote)
 
   
 @main.route('/user/<username>')
 def profile(username):
     user = User.query.filter_by(username=username).first()
-    pitches = user.query.filter_by().first()
-   
-    
-    return render_template('profile.html', user=user,pitches=pitches)
+    # posts = user.pitches.all()
+    posts = user.pitches.order_by(Pitch.timestamp.desc()).all()    
+    return render_template('profile.html', user=user, pitches=posts)
 
 @main.route('/edit-profile', methods = ['GET','POST'])
 @login_required
@@ -50,7 +49,7 @@ def edit():
     form = EditProfile()
     if form.validate_on_submit():
       current_user.username = form.name.data
-      current_user.about_me = form.about_me.data
+      current_user.bio = form.about_me.data
       db.session.add(current_user._get_current_object())
       db.session.commit()
       flash('Your profile has been updated.')
